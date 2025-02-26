45888ce48118b9d00557075d3dfe245b8a604718
Craig Hughes <hghscraig@gmail.com>
2025-02-26 17:34:06 +0000

    chore: install shadcn & remove unneeded files

--- src/mocks/filedata/filedata.test.ts
+++
@@ -1,7 +0,0 @@
-import {test, expect} from 'vitest';
-import { GetData } from '.';
-
-test('check number of files equal 5', async () => {
-    const data = await GetData;
-    expect(data.length).toBe(5);
-})
