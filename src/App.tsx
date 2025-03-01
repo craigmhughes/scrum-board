0870ab4f8ff9de13c609337519d553d0f66ce5f7
Craig Hughes <hghscraig@gmail.com>
2025-03-01 21:01:22 +0000

    chore: amend mobile layout for header styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
             <ActiveCardProvider>
                 <div>
                     <ThemeSwitch />
-                    <div className="flex md:justify-between py-6 my-6 border-b flex-col md:flex-row gap-6 items-center">
+                    <div className="flex md:justify-between py-6 my-6 border-b flex-col md:flex-row gap-6 md:items-center text-left">
                         <div className="flex flex-col text-left">
                             <p className="tracking-tight font-light mb-4">Task Manager</p>
                             <h1 className="tracking-tighter">Your Tasks</h1>
